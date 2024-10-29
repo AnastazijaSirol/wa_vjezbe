1bdc68fe8009f0102d493f9076b83ea3c8d4c9c1
Anastazija Širol <anastazijasirol@gmail.com>
2024-10-29 09:55:38 +0100

    Inicijalni commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 let app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/" + "index.html")
 })
@@ -18,6 +20,58 @@ app.get("/users", (req, res) =>{
     ])
 })
 
+const pizze =[
+    {id: 1, naziv: "Margherita", cijena: 7.00},
+    {id: 2, naziv: "Capriciosa", cijena: 9.00},
+    {id: 3, naziv: "Vegetariana", cijena: 12.00},
+    {id: 4, naziv: "4 sira", cijena: 15.00}
+]
+
+app.get("/pizze", (req, res) => {
+    res.json(pizze)
+})
+
+app.get("/pizze/:id", (req, res) => {
+    let id_pizze = req.params.id;
+    console.log(`korisnik pokašava dohvatit pizzu s id-em ${id_pizze}`)
+
+    if (isNaN(id_pizze)){
+        res.json({message: 'To nije broj'});
+    }
+    for(let pizza of pizze){
+        if(id_pizze==pizza.id){
+            return res.json(pizza);
+        }
+    }
+    res.json({message: 'Nema pizze'})
+})
+
+app.post("/naruci", (req, res) => {
+    const lista = [];
+    const narudzba = req.body;
+    console.log(narudzba);
+    const kljucevi = Object.keys(narudzba);
+    console.log(kljucevi);
+    for(let kljuc of kljucevi) {
+        if (!(kljuc.includes('pizza') && kljuc.includes('velicina') && kljuc.includes('kolicina'))) {
+            res.send('Niste poslali sve potrebne podatke za narudžbu!');
+            return; 
+        }
+        else {
+            lista.push(narudzba);
+            res.send(`Narudžba za pizze: ${narudzba}`)
+        }
+    }
+    res.send(`Vaša narudžba za ${narudzba.pizza} (${narudzba.velicina}) je uspješno
+    zaprimljena!`);
+    for(let kljuc of kljucevi){
+        if (kljuc.pizza != 'Margherita' && kljuc.pizza !='Capriciosa' && kljuc.pizza != 'Vegetariana' && kljuc.pizza != '4 sira'){
+            return res.send("Pizza ne postoji");
+        }
+    }
+
+})
+
 const port=3000;
 
 app.listen(port, (error) => {
@@ -25,5 +79,5 @@ app.listen(port, (error) => {
         console.log("greška")
     } else{
         console.log("slušam")
-    }
+    } 
 });
