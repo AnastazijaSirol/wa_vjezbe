a6d245b6b22920ddabb2bdd5edc7aeb2d1f472ce
AnastazijaSirol <119959886+AnastazijaSirol@users.noreply.github.com>
2024-11-18 16:48:13 +0100

    Update index.js

--- samostalni2/server/index.js
+++ samostalni2/server/index.js
@@ -1,12 +1,10 @@
 import express from "express"
-import cors from 'cors';
 import proizvodiRouter from './routes/proizvodi.js'
 import narudzbeRouter from './routes/narudzbe.js'
 
 const app = express()
 
 app.use(express.json());
-app.use(cors());
 
 app.use("/proizvodi", proizvodiRouter);
 app.use("/narudzbe", narudzbeRouter);
